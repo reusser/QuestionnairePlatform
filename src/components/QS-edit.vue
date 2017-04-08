aa6c7f856a5a07ee2c112c8878f962694223677b
oyh <919917215@qq.com>
2017-04-08 16:32:10 +0800

    add some function

--- src/components/QS-edit.vue
+++ src/components/QS-edit.vue
@@ -1,14 +1,59 @@
 <template>
   <div class="edit-container">
-    <h2>这里是标题</h2>
+    <h2 @click="titleClick" v-if="!titleChange">{{qsItem.title}}</h2>
+    <input type="text" name="qsTitle" v-if="titleChange"
+    v-model="titleValue"
+    @blur="onblur"
+    @keyup.enter="onsubmit"
+    ref="titleInput">
     <div class="content">
-      <div class="add-item">
-        <span class="add-icon">+</span><span>添加问题</span>
+      <div class="questions" v-for="(qs, index) in qsItem.question">
+        <div class="qs-left">
+          <p class="qs-title">
+            {{qs.num}}&nbsp;{{qs.title}}&nbsp;{{getMsg(qs)}}
+          </p>
+          <p v-for="option in qs.options" class="option">
+            <label>
+              <input 
+              type="radio" 
+              :name="`${qs.num}-${qs.title}`"
+              v-if="qs.type === 'radio'">
+              <input 
+              type="checkbox" 
+              :name="`${qs.num}-${qs.title}`"
+              v-if="qs.type === 'checkbox'">{{option}}
+            </label>
+          </p>
+          <textarea 
+          v-if="qs.type === 'textarea'"></textarea>
+        </div>
+        <div class="qs-right">
+          <label><input type="checkbox" :value="qs.isNeed" v-model="qs.isNeed">
+          此题是否必填</label>
+          <p>
+            <span v-if="index !== 0" @click="goUp(index)">上移</span>
+            <span v-if="index !== qsItem.question.length - 1" @click="goDown(index)">下移</span>
+            <span @click="copy">复用</span>
+            <span @click="del">删除</span>
+          </p>
+        </div>
+      </div>
+      <div class="add">
+        <transition name="slide" mode="in-out">
+          <div class="add-option slide-item" key="option" v-if="showBtn">
+            <button>单选</button>
+            <button>多选</button>
+            <button>文本框</button>
+          </div>
+        </transition>
+        <div class="add-item slide-item" key="item" @click="showBtn = !showBtn">
+          <span class="add-icon" >+</span><span>添加问题</span>
+        </div>
       </div>
     </div>
     <footer>
       <span>问卷截止日期</span>
-      <calendar-input :showCalendar="false"></calendar-input>
+      <calendar-input :showCalendar="false" :limit="qsItem.date"></calendar-input>
       <div class="btn-box">
         <button class="save">保存问卷</button>
         <button class="issue">发布问卷</button>
@@ -29,6 +74,87 @@ export default {
   name: 'qsEdit',
   components: {
     calendarInput
+  },
+  data() {
+    return {
+      qsItem: {},
+      qsList: storage.get(),
+      isError: false,
+      showBtn: false,
+      titleChange: false,
+      titleValue: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      if (this.$route.params.num == 0) {
+        this.qsItem.title = '这里是标题'
+        this.qsItem.date  = { 
+          minYear: new Date().getFullYear(),
+          minMonth: new Date().getMonth() + 1,
+          minDay: new Date().getDate(),
+          maxYear: 2030,
+          maxMonth: 3,
+          maxDay: 20
+        }
+        this.qsItem.question = []
+      } else {
+        let i = 0
+        for (let length = this.qsList.length; i < length; i++) {
+          if (this.qsList[i].num == this.$route.params.num) {
+            this.qsItem = this.qsList[i]
+            break
+          }
+        }
+        if ( i === this.qsList.length) this.isError = true
+      } 
+    },
+    getMsg(item) {
+      let msg = ''
+      if (item.type === 'radio') {
+        msg = '(单选题)'
+      } else if (item.type === 'checkbox') {
+        msg = '(多选题)'
+      } else {
+        msg = '(文本题)'
+      }
+
+      return item.isNeed ? `${msg} *` : msg
+    },
+    onblur() {
+      this.titleValue = this.titleValue.trim()
+      this.qsItem.title = this.titleValue === '' ? this.qsItem.title : this.titleValue
+      this.titleChange = false
+    },
+    onsubmit() {
+      this.titleValue = this.titleValue.trim()
+      this.qsItem.title = this.titleValue === '' ? this.qsItem.title : this.titleValue
+      this.titleChange = false
+    },
+    titleClick() {
+      this.titleChange = !this.titleChange
+      setTimeout( () => {
+        this.$refs.titleInput.focus()
+      }, 150 )
+    },
+    goUp(index) {
+
+    },
+    goDown(index) {
+
+    },
+    copy() {
+
+    },
+    del() {
+      
+    }
+  },
+  watch: {
+    '$route': 'fetchData'
   }
 }
 </script>
